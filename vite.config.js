f5c17683f3e76c35d4473e1bc360ae9c641c6c83
TingChunKuo <j033312003@gmail.com>
2025-04-23 04:45:37 +0800

    build setting

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(), 
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
+  base: "/lonely-chat-ai/",
 });
